7216ce261efaeb7212b618316d7139db6401b7cf
saiful201 <118247523+saiful201@users.noreply.github.com>
2024-08-24 20:08:46 +0300

    Update fuck2.js

--- scripts/commands/fuck2.js
+++ scripts/commands/fuck2.js
@@ -71,6 +71,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╭──────•◈•──────╮\n         🄰🄻🅅🄸🄱🄾🅃        \n 🥵______নারী তার শখের পুরুষ কে অসংখ্য স্টাইলে চুদতে দেয়-(👌👈)-💯🤰\n╰──────•◈•──────╯", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╭──────•◈•──────╮\n         𝗦𝗔𝗜𝗙𝗨𝗟-𝗕𝗢𝗧        \n 🥵______নারী তার শখের পুরুষ কে অসংখ্য স্টাইলে চুদতে দেয়-(👌👈)-💯🤰\n╰──────•◈•──────╯", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
       }
